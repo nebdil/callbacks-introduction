2c73339075d3640e8ee37817b7b62087fb9a002a
Dilan <dilannebioglu@gmail.com>
2017-10-05 17:54:05 +0000

    fixed the age issue

--- advSorting.js
+++ advSorting.js
@@ -1,38 +1,24 @@
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 22 },
+  { id: 3, name: "alex",     age: 32 },
   { id: 4, name: "alex",     age: 30 }
 ];
 
-function name(a, b) {
-  if (a.name < b.name) {
-    return -1;
-  }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-}
-
-function age(a, b) {
-  return a.age - b.age;
-}
-
-function byName() {
-  console.log(students.sort(name));
-}
-
-function sorting() {
-  byName();
-  students.forEach(function(e, i) {
-    if(e.name === e.name[i]) {
-      name(e.name, e.name[i]);
+function sorting(arr) {
+  arr.sort(function(a, b) {
+    var nameA = a.name;
+    var nameB = b.name;
+    if (nameA < nameB) {
+      return -1;
     }
-  })
-  }
-sorting();
-
-
-
-
+    if (nameA > nameB) {
+      return 1;
+    }
+    var ageA = a.age;
+    var ageB = b.age;
+    return ageA - ageB;
+    });
+  console.log(arr);
+};
+sorting(students);
